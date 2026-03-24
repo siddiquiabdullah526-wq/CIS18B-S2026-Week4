97aa05585a0998ea24e8b078f48a6fbb33da2443
siddiquiabdullah526-wq <siddiquiabdullah526@gmail.com>
2026-03-24 05:14:05 +0000

    Mini 2: Use finally for cleanup

--- week4-exercises/mini02-finally-cleanup/src/main/java/edu/norcocollege/cis18b/weekx/mini02/FinallyDemo.java
+++ week4-exercises/mini02-finally-cleanup/src/main/java/edu/norcocollege/cis18b/weekx/mini02/FinallyDemo.java
@@ -2,14 +2,14 @@ package edu.norcocollege.cis18b.weekx.mini02;
 
 public class FinallyDemo {
     public static void main(String[] args) {
-        // TODO: Print "Opening alert stream..."
+        System.out.println("Opening alert stream...");
 
         try {
-            // TODO: Intentionally throw a RuntimeException with a helpful message.
+            throw new RuntimeException("Simulated alert stream failure.");
         } catch (RuntimeException ex) {
-            // TODO: Print the error message in a friendly format.
+            System.out.println("Error: " + ex.getMessage());
         } finally {
-            // TODO: Print "Closing alert stream..."
+            System.out.println("Closing alert stream...");
         }
     }
 }
